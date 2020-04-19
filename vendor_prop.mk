65e02cf2b8d7e51a569748d7a72f3aa389d56bf9
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    msm8998-common: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Signed-off-by: 0ranko0P <ranko0p@outlook.com>
    Change-Id: Ic370750b78db0a0734951975dc1fdea98eb38157

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -205,11 +205,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
+# PRODUCT_PROPERTY_OVERRIDES += \
+#    debug.sf.early_phase_offset_ns=1500000 \
+#    debug.sf.early_app_phase_offset_ns=1500000 \
+#    debug.sf.early_gl_phase_offset_ns=3000000 \
+#    debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# Force triple frame buffers
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.early_phase_offset_ns=1500000 \
-    debug.sf.early_app_phase_offset_ns=1500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
