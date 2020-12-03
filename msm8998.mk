b0219ad64db26f0e7b7a8f123edaf62a6e7d330e
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    msm8998-common: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    [SebaUbuntu] Remove manifest entry as well
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- msm8998.mk
+++ msm8998.mk
@@ -150,6 +150,10 @@ PRODUCT_PACKAGES += \
     libpiex_shim \
     vendor.qti.hardware.camera.device@1.0:64
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
