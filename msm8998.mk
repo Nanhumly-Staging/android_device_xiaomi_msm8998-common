f4975627c9e3cdc248b2d8b0ad4533672a987028
Timi Rautamäki <timi.rautamaki@gmail.com>
2021-10-15 11:54:56 +0000

    msm8998-common: switch to QCRIL
    This avoids an early RIL crash resulting in inresponsive SIM cards.
    
    Change-Id: I5cbd3fa85f412befc37bd4db46a29c9f0c87c743

--- msm8998.mk
+++ msm8998.mk
@@ -372,12 +372,13 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0.vendor \
-    android.hardware.radio.config@1.0.vendor \
-    android.hardware.secure_element@1.0.vendor \
+    android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.2.vendor \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.hardware.secure_element@1.2.vendor \
     CarrierConfigOverlay \
-    libril-wrapper \
-    librmnetctl
+    librmnetctl \
+    libxml2
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
