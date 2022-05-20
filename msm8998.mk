560b2649f6e7102182975e5199e2bbf3c06373eb
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:40:27 +0100

    msm8998-common: Preopt SystemUI for AOSP builds.
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: Icc38efb4b702b518caa22a3abfc7c04d6ba1ac2a

--- msm8998.mk
+++ msm8998.mk
@@ -164,6 +164,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
