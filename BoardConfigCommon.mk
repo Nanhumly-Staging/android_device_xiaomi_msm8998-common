7707dfd12741e348a90a342fb263be1a1ac5fc2a
Bruno Martins <bgcngm@gmail.com>
2022-07-04 23:42:23 +0100

    msm8998-common: Define main kernel defconfig
    Targets will append config fragments.
    
    Change-Id: I744d56d714add1c1864746276c554bdb5086a7a9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,6 +41,7 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_CONFIG := vendor/xiaomi/mi8998_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8998
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
