a42e5446e6203a26eff3051884abf68d1fd0a8bd
Bruno Martins <bgcngm@gmail.com>
2022-09-23 07:16:50 +0000

    msm8998-common: bluetooth: Undefine BTM_DEF_LOCAL_NAME
    It has been migrated to bluetooth.device.default_name sysprop.
    
    Change-Id: I1cd5472fa96253952fa65d0154211c78e4f8c14b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,21 +24,6 @@
 #include <cutils/properties.h>
 #include <string.h>
 
-static inline const char* BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-
-    if (strstr(product_device, "chiron"))
-        return "Xiaomi Mi MIX 2";
-    if (strstr(product_device, "sagit"))
-        return "Xiaomi MI 6";
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
