6b8bb639ee95df4bfe6a02f133e6c0ed34f3f5ff
Michael Bestas <mkbestas@lineageos.org>
2022-10-30 19:01:36 +0200

    msm8998-common: Move qti_whitelist.xml to /system_ext
    Change-Id: Id8617513cfd686b3f6888a89c1b1db196acfb476

--- msm8998.mk
+++ msm8998.mk
@@ -331,7 +331,7 @@ PRODUCT_PACKAGES += \
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml
 
 # QMI
 PRODUCT_PACKAGES += \
