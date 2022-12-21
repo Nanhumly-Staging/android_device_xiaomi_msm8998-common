ffa98a4128d727f6dd33d12bf9b9b936285a9806
Nanhumly <1607616422@qq.com>
2022-12-21 10:32:59 +0000

    msm8998-common: Add unofficial OTA update support
    Change-Id: I0ef3227b586b1a3d37f943e288f988514eb1af14

--- msm8998.mk
+++ msm8998.mk
@@ -390,6 +390,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Updater
+PRODUCT_PACKAGES += \
+    UpdaterOverlay
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
