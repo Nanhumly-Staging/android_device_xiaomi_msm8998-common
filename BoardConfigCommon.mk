03a06d925b29cfcb065d1cd7fbf00d6f6862e4cc
Nanhumly <admin@warbler.cc>
2023-03-06 12:13:23 +0000

    msm8998-common: Migration to EAS
    Change-Id: Ia182a570114ff47c4b9b2839b407f2f4800139f8
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -103,7 +103,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_PRODUCT_PROP += $(PLATFORM_PATH)/product.prop
