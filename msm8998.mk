e1c2836d10ebb7475244690dfa6151357ea609ee
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-14 01:23:39 +0200

    msm8998-common: Build Xiaomi sensors impl
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8998.mk
+++ msm8998.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2023 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -387,7 +387,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl:64 \
+    android.hardware.sensors@1.0-impl-xiaomi:64 \
     android.hardware.sensors@1.0-service
 
 # Soong namespaces
