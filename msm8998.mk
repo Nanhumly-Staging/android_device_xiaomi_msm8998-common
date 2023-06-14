bf93e9b03e0aa859c2ca73bba402d1782b4db131
someone5678 <nemui3353@gmail.com>
2023-06-14 11:22:34 +0300

    msm8998-common: Create dummy libldacBT_bco
    * This target is never existed and Bluetooth apex module also doesn't actually use it.
      But module still opening the library. Which cause log spam.
    
    * This commit is not required if you can just edit the source.
    
    a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:135 A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I8edfd11a7a87c277d01487219650bb30bfa9f46a
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8998.mk
+++ msm8998.mk
@@ -135,6 +135,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
