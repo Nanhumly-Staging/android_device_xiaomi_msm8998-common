fcfcb0aba3b1d22ef964a8a0053fc83b76861626
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    msm8998-common: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- msm8998.mk
+++ msm8998.mk
@@ -346,7 +346,8 @@ PRODUCT_PACKAGES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # QCOM
 PRODUCT_COPY_FILES += \
