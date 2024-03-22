536e514a68c7fb7b2cc73b890f000228ccf40978
Nanhumly <admin@warbler.cc>
2024-03-22 17:11:23 +0000

    msm8998-common: Update ANT+ and Bluetooth blobs from FairPhone 3
    * From FP3-REL-6.A.023.1-gms-497e9bef-user
    
    Change-Id: I93a8f57d3968bbad021b0f1fa58bac1615aeab54
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8998.mk
+++ msm8998.mk
@@ -131,8 +131,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
     libldacBT_bco \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
+    vendor.qti.hardware.btconfigstore@2.0.vendor:64
 
 # Camera
 PRODUCT_PACKAGES += \
