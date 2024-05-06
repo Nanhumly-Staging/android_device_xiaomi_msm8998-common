33e496065c77ea1f48b5fcff159c24dc178f21d8
Nanhumly <admin@warbler.cc>
2024-05-06 16:30:10 +0000

    msm8998-common: Update FairPhone 3 blobs from FP3-REL-6.A.028.1-gms-fb581341-user
    Change-Id: I5596ae7313a39726c779450762a199c14102e7f7
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -125,7 +125,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-12-05
+VENDOR_SECURITY_PATCH := 2024-04-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
