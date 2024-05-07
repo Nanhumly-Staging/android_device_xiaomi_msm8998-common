30bce04c19de97abff97a36b49e3dd99e9998ec2
Nanhumly <admin@warbler.cc>
2024-05-07 10:40:16 +0000

    msm8998-common: Switch to FairPhone 3 DRM (Widevine) blobs
    * From FP3-REL-6.A.028.1-gms-fb581341-user
    
    Change-Id: I9065f60f481a43e78b6cc3c71d49b7da80d01872
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- extract-files.sh
+++ extract-files.sh
@@ -107,9 +107,6 @@ function blob_fixup() {
                 sed -i "s|android.hardware.radio.config@${v}.so|android.hardware.radio.c_shim@${v}.so|g" "${2}"
             done
             ;;
-        vendor/lib64/libwvhidl.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-            ;;
         vendor/lib64/hw/keystore.msm8998.so)
             sed -i "s|keystore.msm8953.so|keystore.msm8998.so|g" "${2}"
             ;;
