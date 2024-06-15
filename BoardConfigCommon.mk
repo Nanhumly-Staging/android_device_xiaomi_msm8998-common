f18028ee4fe19383bff6812f0b7c5e192a9b78e5
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 23:43:37 +0300

    msm8998-common: Add a device specific FCM
    Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,7 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(PLATFORM_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
