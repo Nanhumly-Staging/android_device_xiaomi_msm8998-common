74385bb88fe5ce716a5ac4023256bcbfc51a2d83
Bruno Martins <bgcngm@gmail.com>
2024-06-16 15:14:57 +0100

    msm8998-common: Inherit from QTI and common Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,6 +78,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
